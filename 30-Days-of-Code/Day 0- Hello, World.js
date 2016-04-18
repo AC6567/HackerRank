b4d85098980c7850fea358d86db3696e7d214edf
Perrin Clark <blackguycoding@gmail.com>
2016-04-18 12:02:01 +0300

    create new solution

---
+++ 30-Days-of-Code/Day 0- Hello, World.js
@@ -0,0 +1,23 @@
+/* Day 0: Hello, World.
+HackerRank 30 Days of Code by Perrin Clark
+BlackGuyCoding@gmail.com
+*/
+
+function processData(inputString) {
+    // Your first line of output goes here
+    process.stdout.write("Hello, World.");
+
+    // Write the second line of output
+}
+
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
