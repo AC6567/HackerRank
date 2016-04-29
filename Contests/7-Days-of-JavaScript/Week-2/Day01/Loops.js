461df47b88e8ef49fe8a26e59421cb7227a440e7
Perrin Clark <blackguycoding@gmail.com>
2016-04-29 20:31:35 +0300

    added solution

---
+++ Contests/7-Days-of-JavaScript/Week-2/Day01/Loops.js
@@ -0,0 +1,19 @@
+function vowelsAndConsonants(s) {
+
+    var Arr = s.toLowerCase().split( '' ),
+    c = '',
+    vArr = ['a','e','i','o','u'],
+    vowelsResult = "",
+    consonantsResult = "";
+
+    for ( c of Arr ){
+        if( vArr.indexOf( c )!==-1 ){
+            vowelsResult += c + "\n";
+        }
+        else {
+            consonantsResult += c + "\n";
+        }
+    }
+
+    console.log(vowelsResult+consonantsResult);
+}
