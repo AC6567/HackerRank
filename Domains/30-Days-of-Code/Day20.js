efdd1fe54044ae646c1c8ec1b7afaf2468e3b39d
BlackGuyCoding@gmail.com <Blackguycoding@gmail.com>
2016-05-20 16:17:44 +0300

    added new solution

---
+++ Domains/30-Days-of-Code/Day20.js
@@ -0,0 +1,20 @@
+/* Day 20: Sorting
+
+HackerRank 30 Days of Code by Perrin Clark
+BlackGuyCoding@gmail.com
+*/
+
+function processData(input) {
+    //Enter your code here
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
